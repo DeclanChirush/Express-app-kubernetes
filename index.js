d6034d7930d99693c9b5f8126d9c89265504278c
Hirush Gimhan <it19240848@my.sliit.lk>
2022-05-27 00:14:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-    res.send("<h1> #GOHOMEGOTA </h1>");
+    res.send("<h1> #GOHOMEGOTA #GIVEUSOURSTOLENMONEY #UPDATED </h1>");
 });
 
 app.listen(port, () => {
